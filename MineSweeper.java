f900a0ee43f385c9109ba15379ee0c69d135e7b9
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-05-01 14:14:24 -0400

    Update MineSweeper.java

--- MineSweeper.java
+++ MineSweeper.java
@@ -2,6 +2,9 @@ public class MineSweeper
 {
     int[][] board;
     String[][] playerBoard;
+    String[] letterArray = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+     String[] numArray = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"};
+    Integer[] intsArray = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 
     public void generateBoard(int numMines, int row, int column )
     {
@@ -72,8 +75,7 @@ public class MineSweeper
     public void start(int rows, int columns)
 {
    String[][] playerBoard = new String[rows+1][columns+1];
-   String[] letterArray = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-   String[] numArray = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"};
+   
    
    
    
@@ -94,8 +96,8 @@ public class MineSweeper
     }
  }
     
-    public void enterCord(int row, int column)
-    {Integer[] intsArray = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    public void enterCord(String row, int column)
+    {
      
           
     }
