861be2cac109f273cff907b1ab0a3b3ddb26256d
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-05-01 10:50:31 -0400

    Update MineSweeper.java

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,83 +1,34 @@
-public class MineSweeper
+public class Test
 {
-    int[][] board;
-
-    public void generateBoard(int numMines, int row, int column )
-    {
-         int[][] mines = new int[numMines][2];
-         for (int i=0; i<numMines; i++)
-         {
-             mines[i][0] = (int)(Math.random() *(row-1)+1)-1;
-             mines[i][1] = (int)(Math.random()*(column-1)+1)-1;
-         }
-         for (int[] n: mines)
-         {
-             board[n[0]][n[1]] = 9;
-         }
-         
-         for(int i=0; i<row; i++)
-         {
-             for (int j=0; j<column; j++)
-             {
-                 if (board[i][j] != 9)
-                 {
-                 board[i][j] = checkMines(i,j);
-                }
-             }
-         }
-         
-    }
+    public int counter = 0;
     
-    public MineSweeper(int row, int column)
+    public static void main()
     {
-     board = new int[row][column];
-     generateBoard(4, row, column);
-    }
-    
-    public int checkMines(int row, int column)
-    {
-        int out = 0;
-        int[][] placeChecks  = {{row-1, column-1},{row-1,column},{row-1,column+1},{row,column+1},{row+1,column+1},{row+1,column},
-                      {row+1,column-1},{row, column-1}};
-        
-        for(int[] n: placeChecks)
-        {
-             if(n[0] >= 0 && n[1]>=0 && n[0] <= 4 && n[1]<=4 )
-             {
-                 if(board[n[0]][n[1]]==9)
-                 {
-                      out+=1;
-                 }
-             }
-              
-        }
-        return out;
-    }
-    
-    public void printGame()
-    {
-        
-        for(int[] n: board)
-        {
-            String out = "";
-            for(int m: n)
-            {
-                out = out+m+" ";
-            }
-            System.out.println(out + "\n");
-            out = "";
-        }
-    }
-
+    /*int row = 5;
+    int column = 5;
+    MineSweeper round = new MineSweeper(row, column, 4);
+    round.printGame();
+    start(row,column);*/
 }
 
-
-/*
- * for (int i=0; i<=5; i++)
-        {
-            for (int j=0; j<=5; j++)
-            {
-                board[i][j] = false;
-            }
-        }
- */
+public void start(int rows, int columns)
+{
+   String[][] playerBoard = new int[rows+1][columns+1];
+   String[] letterArray = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+   String[] numArray = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"};
+   
+   
+   for (int i=0; i<columns; i++)
+   {
+       playerBoard[0][i] = i+1;
+    }
+   for (int i=0; i<rows; i++)
+   {
+       playerBoard[i][0] = letterArray[i];
+   }
+   for(int i=1; i<rows;i++)
+   {
+    
+    }
+ }
+}
